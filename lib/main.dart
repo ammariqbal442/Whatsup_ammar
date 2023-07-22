6b21605b439c5a982507af5d38078e09e4447e48
ammariqbal442 <103180263+ammariqbal442@users.noreply.github.com>
2023-07-22 16:26:26 +0500

    added firebase optional file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsupp_application/firebase_options.dart';
 
 import 'package:whatsupp_application/screens/mobile_layout_screen.dart';
 import 'package:whatsupp_application/screens/web_layout_screen.dart';
@@ -6,7 +8,11 @@ import 'package:whatsupp_application/utils/responsive_layout.dart';
 
 import 'colors.dart';
 
-void main() {
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
